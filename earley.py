0264f55ba33e2acc1797894031e9b8cf66433b28
Sagie Maoz <sagie@maoz.info>
2011-07-19 16:41:26 +0300

    move to run()

--- earley.py
+++ earley.py
@@ -7,23 +7,27 @@ from grammar import *
 from sentence import *
 from parser import *
 
-# TODO handle sentence from stdin
-if len(sys.argv)<3:
-    print "Usage: earley.py <grammar.cfg> <sentence>"
-    sys.exit(1)
-
 def print_trees(trees):
     for tree in trees:
         print tree
 
-# load grammar from file, sentence from arguments
-grammar = Grammar.from_file(sys.argv[1])
-sentence = Sentence.from_string(sys.argv[2])
 
-# run parser
-earley = Parser(grammar, sentence)
-earley.parse()
-#trees = earley.parse()
+def run():
+    if len(sys.argv)<3:
+        print "Usage: earley.py <grammar.cfg> <sentence>"
+        sys.exit(1)
 
-# print results
-# print_trees(trees)
+    # load grammar from file, sentence from arguments
+    grammar = Grammar.from_file(sys.argv[1])
+    sentence = Sentence.from_string(sys.argv[2])
+
+    # run parser
+    earley = Parser(grammar, sentence)
+    earley.parse()
+    #trees = earley.parse()
+
+    # print results
+    # print_trees(trees)
+
+if __name__ == '__main__':
+    run()

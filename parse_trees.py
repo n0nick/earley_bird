379155357191d93a6d4d559afca93b0e8c9aaad6
n0nick <sagiem@gmail.com>
2011-07-20 16:36:01 +0300

    parse trees?

--- parse_trees.py
+++ parse_trees.py
@@ -9,40 +9,51 @@ class TreeNode:
         self.body = body
         self.children = children
 
-    def leaves_count(self):
+    def __len__(self):
         if self.is_leaf():
             return 1
         else:
-            return reduce(add, [child.leaves_count() for child in self.children])
+            return reduce(add, [len(child) for child in self.children])
+
+    def __str__(self):
+        st = "[.{0} ".format(self.body)
+        if not self.is_leaf():
+            st+= ' '.join([str(child) for child in self.children])
+        st+= ' ]'
+        return st
 
     def is_leaf(self):
-        return self.children == []
+        return len(self.children) == 0
 
 class ParseTrees:
     def __init__(self, routes, length, debug):
         self.routes = routes
-        self.nodes = [list() for i in range(length)]
-        self.trees = []
         self.length = length
+        self.nodes = []
+        self.root = self.find_root()
 
-#        self.build_trees(debug)
+        self.nodes = self.build_nodes(self.root)
 
-    def build_nodes(self, debug=None):
-        for row in self.routes:
-            rule = row.rule
-            children = [TreeNode(s) for s in rule.rhs]
-            node = TreeNode(rule.lhs, children)
-            self.nodes[rule.start].append(node)
-        if debug:
-            print "Created nodes:"
-            print self.nodes
-    
-    def build_trees(self, nodes, debug=None):
-        self.build_nodes(debug)
-        trees = None #TODO
-        res = []
-        if trees:
-            for tree in trees:
-                if tree.leaves_count == self.length:
-                    res.append(tree)
-        return res
+    def __str__(self):
+        st = []
+        for i in range(len(self.nodes)):
+            st.append("Parse tree #{0}:\n{1}\n\n".format(i+1, str(self.nodes[i])))
+        return '\n'.join(st)
+
+    def find_root(self):
+        from parser import Parser
+        return self.routes[Parser.GAMMA_SYMBOL]
+
+    def build_nodes(self, root):
+        nodes = []
+
+        for r in root:
+            children = []
+            for child in r.rule.rhs:
+                if child in self.routes:
+                    children.extend(self.build_nodes(self.routes[child]))
+
+            node = TreeNode(r.rule.lhs, children)
+            nodes.append(node)
+        
+        return nodes
